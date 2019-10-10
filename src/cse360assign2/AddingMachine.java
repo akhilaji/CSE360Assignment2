e9eb9895e03c3c2eefb70eaa2101707751ea225e
Akhil Aji <akhilaji99@gmail.com>
2019-10-09 20:23:36 -0700

    Added Documentation for initial commit

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -1,29 +1,70 @@
+/**
+ * Class ID: 70641
+ * Assignment 2 
+ * Description: This class implements functions to add and subtract the parameter value from the total.
+ * 
+ * @author Akhil Aji
+ * @version 1.0
+ * 
+ */
+
 package cse360assign2;
 
+/**
+ * The class AddingMachine adds and subtracts values from an initial starting value of 0 which is 
+ * represented by the total value. This is presented to the user via the toString method.
+ * 
+ * @author akhilaji
+ *
+ */
 public class AddingMachine {
 
 	private int total;
 	
+	/**
+	 * Default constructor for the AddingMachine class.
+	 * initializes the total value to 0
+	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * This is a getter method to return the total.
+	 * @return the total value after the operations
+	 */
 	public int getTotal () {
 		return 0;
 	}
 	
+	/**
+	 * This method adds a Int value to the total. 
+	 * @param This parameter has the value to be added
+	 */
 	public void add (int value) {
 		
 	}
 	
+	/**
+	 * This method subtracts a Int value from the total
+	 * @param This parameter has the value to be 
+	 * 
+	 */
 	public void subtract (int value) {
 		
 	}
-		
+	
+	/**
+	 * This method returns the computed value
+	 * @return computed value as a string
+	 */
 	public String toString () {
 		return "";
 	}
 
+	/**
+	 * This method clears the string
+	 */
 	public void clear() {
 	
 	}
